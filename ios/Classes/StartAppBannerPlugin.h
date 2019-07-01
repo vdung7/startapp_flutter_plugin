53b3d9608cdaf6875bd50af453495816502af070
Dung Vu <dungvu@rta.vn>
2019-07-01 21:31:57 +0700

    chore: add lacking files for ios build

---
+++ ios/Classes/StartAppBannerPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface StartAppBannerPlugin: NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/StartAppBannerPlugin.m
@@ -0,0 +1,20 @@
+#import "StartAppBannerPlugin.h"
+
+@implementation StartAppBannerPlugin 
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"startapp"
+            binaryMessenger:[registrar messenger]];
+  StartAppBannerPlugin* instance = [[StartAppBannerPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
