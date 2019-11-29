6661441872d932a56b94171d6be09f77ee0030cf
Dung Vu <dungvu@rta.vn>
2019-11-29 13:19:49 +0700

    config: prepare for publish to flutter pub

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.0
+
+* Support load interstitial ad method
+
 ## 0.0.1
 
 * TODO: Describe initial release.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:startapp/banner.dart';
+import 'package:startapp/startapp.dart';
 
 void main() => runApp(MyApp());
 
